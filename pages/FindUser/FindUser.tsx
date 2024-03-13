bacb82bd9895117d3b04a64c9f680a8c76894d10
hotteokButler <ygs12567@gmail.com>
2024-03-13 17:30:18 +0900

    Design : 추가 findToggle component
    -findtoggle conponent UI 추가 구현 필요
    -input data test 끝나면 분리 필요

--- pages/FindUser/FindUser.tsx
+++ pages/FindUser/FindUser.tsx
@@ -5,7 +5,7 @@ export default function FindUser() {
   return (
     <>
       <p className='find_info'>* 아이디를 찾을 방법을 선택해 주세요.</p>
-      <FindToggle/>
+      <FindToggle toggle_title='등록된 이메일로 찾기'/>
     </>
   );
 }
--- src/components/FindToggle.tsx
+++ src/components/FindToggle.tsx
@@ -1,16 +1,72 @@
 import React, { Dispatch, SetStateAction } from 'react';
+import { IoIosArrowDown } from 'react-icons/io';
+import { ChangeHandler, RegisterOptions, SubmitHandler, useForm } from 'react-hook-form';
 
-export interface IFindToggle {
+interface IFindToggleForm {
+  user_id?: string;
+  user_name?: string;
+  email?: string;
+  phone?: number | string;
+}
+
+interface IFindInputObj {
+  name: keyof IFindToggleForm;
+  required?: boolean;
+  key?: number;
+  type?: string;
+  placeholder?: string | undefined;
+}
+
+interface IFindToggle {
+  toggle_title: string;
+  find_input_data?: Array<IFindInputObj>;
   setToggleAction?: Dispatch<SetStateAction<boolean>>;
 }
 
-export default function FindToggle(props: IFindToggle) {
-  const { setToggleAction } = props;
+const findTestObj: Array<IFindInputObj> = [
+  {
+    type: 'text',
+    name: 'user_name',
+    placeholder: '이름',
+  },
+  {
+    type: 'email',
+    name: 'email',
+    placeholder: '이메일',
+  },
+];
 
+export default function FindToggle(props: IFindToggle) {
+  const { toggle_title, setToggleAction, find_input_data } = props;
+  const {
+    register: find_register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<IFindToggleForm>({ mode: 'onChange' });
+
+  const onSubmit: SubmitHandler<IFindToggleForm> = () => {};
   return (
     <>
       <div className='find_toggle_con'>
-        
+        <h4 className='toggle_title'>
+          {toggle_title}
+          <button type='button'>
+            <IoIosArrowDown />
+          </button>
+        </h4>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {findTestObj &&
+            findTestObj.map((elem, idx) => (
+              <input
+                key={idx}
+                type={elem.type || 'text'}
+                placeholder={elem.placeholder || ''}
+                {...find_register(`${elem.name}`, { validate: (val) => (val ? true : '필수입력값입니다') })}
+              ></input>
+            ))}
+          <button type='submit'>확인</button>
+        </form>
       </div>
     </>
   );
